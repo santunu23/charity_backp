7bca1c2878e099e87c7aec69ab40d32b490f7e9c
santunu23 <santunu23@gmail.com>
2019-04-16 15:15:49 +0600

    commit on 16th april

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,30 @@
+import { AuthGuard } from './auth.guard';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'homeload', pathMatch: 'full' },
+  { path: 'homeload',
+  children: [
+    {
+      path: '',
+      loadChildren: './homeload/homeload.module#HomeloadPageModule' 
+    },
+   
+    {
+      path: ':homeloadId',
+      loadChildren: './homeload/homeload-details/homeload-details.module#HomeloadDetailsPageModule'
+    },
+  ]
+  },
+  { path: 'getupdate', loadChildren: './getupdate/getupdate.module#GetupdatePageModule' },
+  { path: 'donation', loadChildren: './donation/donation.module#DonationPageModule' },
+  { path: 'volunteer', loadChildren: './volunteer/volunteer.module#VolunteerPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'payment', loadChildren: './payment/payment.module#PaymentPageModule' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'forgetpword', loadChildren: './forgetpword/forgetpword.module#ForgetpwordPageModule' },
 ];
 
 @NgModule({
